c7658eafeac75c2cd0535f20ce5abd6518bcb4e8
Singh2655 <115347390+Singh2655@users.noreply.github.com>
2023-07-23 21:12:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {CTA,Brand,Navbar} from './components'
+import {Blog,Features,Footer,Header,Possibility,WhatGPT} from './containers'
+import './App.css'
+function App() {
+  return (
+    <div className='App'>
+      <div className="gradient__bg">
+        <Navbar/>
+        <Header/>
+      </div>
+      <Brand/>
+      <WhatGPT/>
+      <Features/>
+      <Possibility/>
+      <CTA/>
+      <Blog/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
+
